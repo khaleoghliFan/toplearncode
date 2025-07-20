d1fd319dd8963b4d8b35d62677d7a704d371db0a
Khaleoghli <fanavakhaleoghli@gmail.com>
2025-07-20 12:06:33 +0330

    session Dynamic urls

--- session2/MyProject/challenges/urls.py
+++ session2/MyProject/challenges/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("hello/", views.hello_world), ]
+urlpatterns = [path("hello/", views.hello_world),
+               path("<day>/",views.weekdays),
+               ]
--- session2/MyProject/challenges/views.py
+++ session2/MyProject/challenges/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+#data:
+days={"Monday":"today is monday",
+      "Tuesday":"today is tuesday",
+      "Wednesday":"today is wednesday",
+      "Thursday":"today is thursday",
+      "Friday":"today is friday",
+      "Saturday":"today is saturday",
+      "Sunday":"today is sunday",}
 def hello_world(request):
     return HttpResponse("Hello World!")
+def weekdays(request,day):
+    data_days=days.get(day)
+    return HttpResponse(f"day is {data_days}")
+
 
 
